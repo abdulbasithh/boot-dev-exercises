f566b30bc790d3513d2a33da9a3f2891fa08c412
Abdul <31511194+abdulbasithh@users.noreply.github.com>
2026-02-25 15:37:23 +0530

    fix the logic with the addition and completed the program to add updated values based on user input

--- Back-end Developer Path (Python & Go)/changing_in_place/main.py
+++ Back-end Developer Path (Python & Go)/changing_in_place/main.py
@@ -1,3 +1,4 @@
 def update_player_score(current_score, increment):
-    increment = current_score + increment
+    # made a mistake of default the value to 0 always, now corrected
+    current_score = current_score + increment
     return current_score
