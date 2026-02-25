c5997b2c17803fe4d46d69df2c6b235b47c75b9a
Abdul <31511194+abdulbasithh@users.noreply.github.com>
2026-02-25 19:29:52 +0530

    completed scientific notation showcase using e

---
+++ Back-end Developer Path (Python & Go)/scientific_notation/main.py
@@ -0,0 +1,8 @@
+def max_players_on_server():
+    small_server = 1.024e18
+    medium_server = 1.024e19
+    large_server = 1.024e20
+    #print(small_server,medium_server,large_server)
+    return small_server, medium_server, large_server
+
+max_players_on_server()
